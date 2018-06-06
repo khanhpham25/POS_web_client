c944af4d5d9c58cf5f63318ff8a9c5f25563cf39
khanhpham <khanh.pb25@gmail.com>
2018-06-07 01:50:45 +0700

    Remove Sidebar Tab

--- src/layouts/Dashboard/Sidebar/index.js
+++ src/layouts/Dashboard/Sidebar/index.js
@@ -52,10 +52,6 @@ class Sidebar extends Component {
               <span>Customers</span>
             </NavLink>
           </Menu.Item>
-          <Menu.Item key='2'>
-            <Icon type='desktop' />
-            <span>Option 2</span>
-          </Menu.Item>
           <SubMenu
             key='sub1'
             title={<span><Icon type='user' /><span>User</span></span>}
@@ -64,17 +60,6 @@ class Sidebar extends Component {
             <Menu.Item key='4'>Bill</Menu.Item>
             <Menu.Item key='5'>Alex</Menu.Item>
           </SubMenu>
-          <SubMenu
-            key='sub2'
-            title={<span><Icon type='team' /><span>Team</span></span>}
-          >
-            <Menu.Item key='6'>Team 1</Menu.Item>
-            <Menu.Item key='8'>Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key='9'>
-            <Icon type='file' />
-            <span>File</span>
-          </Menu.Item>
         </Menu>
       </Sider>
     );
