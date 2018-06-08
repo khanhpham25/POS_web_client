3924e93b4bc324e3bb0fe2bfccb3f8985336702b
khanhpham <khanh.pb25@gmail.com>
2018-06-08 08:47:07 +0700

    Remove Perfect scroll

--- src/layouts/Dashboard/index.js
+++ src/layouts/Dashboard/index.js
@@ -2,7 +2,6 @@ import React, { Component, Fragment } from 'react';
 
 import { Switch, Route, Redirect } from 'react-router-dom';
 // creates a beautiful scrollbar
-import PerfectScrollbar from 'perfect-scrollbar';
 
 import AppHeader from './Header';
 import Sidebar from './Sidebar';
@@ -10,11 +9,6 @@ import AppFooter from './Footer';
 import { Layout } from 'antd';
 
 class DashboardLayout extends Component {
-  componentDidMount() {
-    if (navigator.platform.indexOf('Win') > -1) {
-      const ps = new PerfectScrollbar(this.refs.mainPanel);
-    }
-  }
   componentDidUpdate() {
     //this.refs.mainPanel.scrollTop = 0;
   }
